ab95314ab37f60758a3b65beabe3bdce4e692eec
Your Name <you@example.com>
2018-02-13 18:58:59 +0530

    initial setup

---
+++ bin/configx.js
@@ -0,0 +1,8 @@
+jso={dburl: 'mongodb://admin:admin@ds141796.mlab.com:41796/cms', hotel_name: 'Any Hotel'}
+module.exports = {
+ maindetails: function() {
+ 	return  jso;      
+    }    
+};
+//
+//mongodb://admin:admin@ds227858.mlab.com:27858/cms
